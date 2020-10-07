4b15816c4d0d45a7293ffa8a853fdb160568d3c6
nugaa <ricardonuga@gmail.com>
2020-10-07 18:16:03 +0100

    ChatMe commit: início de upload de img

--- lib/networking/firebase_storage.dart
+++ lib/networking/firebase_storage.dart
@@ -3,5 +3,4 @@ import 'package:firebase_storage/firebase_storage.dart';
 class FirebaseStorageRepo {
   final FirebaseStorage _storage =
       FirebaseStorage(storageBucket: 'gs://chatme-a5cb8.appspot.com/');
-
 }
